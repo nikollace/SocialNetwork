d99048e51369317b86c830a0df9a77f4ef1a0f4f
iNikollace <niikollace@gmail.com>
2021-06-19 15:36:42 +0200

    Sredjena struktura, dodate rute za get i create posts.

--- server/index.js
+++ server/index.js
@@ -3,3 +3,28 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 import dotenv from 'dotenv';
 
+import postRoutes from './routes/posts.js';
+
+const app = express();
+
+dotenv.config();
+
+//Sve rute iz routes/posts.js morace da idu na localhost:5000/posts
+app.use('/posts', postRoutes);
+
+app.use(express.json({ limit: "30mb", extended: true }));
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
+
+app.use(cors());
+
+// app.get('/', (req, res) => {
+//     res.send('Hello to memories API!');
+// });
+
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .catch((error) => console.log(error.message));
+
+mongoose.set('useFindAndModify', false);
