47b9a68e2970eebfdc5ed9771d1cc916f1940298
iNikollace <niikollace@gmail.com>
2021-06-20 12:37:45 +0200

    Dodat react router i rute, treba srediti login formu

---
+++ client/src/components/home/Home.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from 'react'
+import { Grow, Grid, Container } from '@material-ui/core';
+import { useDispatch } from 'react-redux';
+import { getPosts } from '../../actions/posts';
+import Posts from '../Posts/Posts';
+import Form from '../Form/Form';
+import useStyles from './styles';
+
+const Home = () => {
+
+    const classes = useStyles();
+    const dispatch = useDispatch();
+
+    const [currentId, setCurrentId] = useState(null);
+
+    //currentId ovde ide u uglaste jer cemo kada u clear funkciji u Form.js 
+    // postaviti currId na null to je promena ovde useEffect hvata
+    // i trebalo bi da fetch-uje postove opet
+    useEffect(() => {
+        dispatch(getPosts());
+    }, [currentId, dispatch]);
+
+    return (
+        <Grow in>
+                <Container>
+                    <Grid className={classes.mainContainer} container justify="space-between" alignItems="stretch" spacing={3}>
+                        <Grid item xs={12} sm={7}>
+                            <Posts setCurrentId={setCurrentId}/>
+                        </Grid>
+
+                        <Grid item xs={12} sm={4}>
+                            <Form currentId={currentId} setCurrentId={setCurrentId}/>
+                        </Grid>
+                    </Grid>
+                </Container>
+            </Grow>
+    )
+}
+
+export default Home
