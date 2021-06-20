e3fce559f49b6d35e3782cee747558e740884e1d
iNikollace <niikollace@gmail.com>
2021-06-20 10:13:28 +0200

    Odradjen update, dodati samo get nakon update da vrati nove postove

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useState, useEffect } from 'react'
 import {Container, AppBar, Typography, Grow, Grid} from '@material-ui/core';
 
 import { useDispatch } from 'react-redux';
@@ -11,6 +11,8 @@ import useStyles from './styles';
 const App = () => {
     const classes = useStyles();
     const dispatch = useDispatch();
+
+    const [currentId, setCurrentId] = useState(null);
 
     useEffect(() => {
         dispatch(getPosts());
@@ -27,11 +29,11 @@ const App = () => {
                 <Container>
                     <Grid container justify="space-between" alignItems="stretch" spacing={3}>
                         <Grid item xs={12} sm={7}>
-                            <Posts />
+                            <Posts setCurrentId={setCurrentId}/>
                         </Grid>
 
                         <Grid item xs={12} sm={4}>
-                            <Form />
+                            <Form currentId={currentId} setCurrentId={setCurrentId}/>
                         </Grid>
                     </Grid>
                 </Container>
