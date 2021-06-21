17b2ff5f3811fe5a8dc84fbcfa0eb8aab271c5dd
MirkoZecic <55135159+MirkoZecic@users.noreply.github.com>
2021-06-21 11:36:25 +0200

    edit i delete se vide samo ukoliko smo kreatori, takodje lep prikaz lajkova

--- server/models/postMessage.js
+++ server/models/postMessage.js
@@ -3,11 +3,12 @@ import mongoose from 'mongoose';
 const postSchema = mongoose.Schema({
     title: String,
     message: String,
+    name: String,
     creator: String,
     tags: [String],
     selectedFile: String,
     likes: {
-        type: [String], 
+        type: [String],
         default: []
     },
     createdAt: {
