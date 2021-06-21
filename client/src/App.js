d07f5e97496836255bdcba86dc06a5dbadf56b96
iNikollace <niikollace@gmail.com>
2021-06-21 12:55:56 +0200

    Sredjene redirekcije

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,7 @@ import Auth from './components/Auth/Auth';
 import PostDetails from './components/PostDetails/PostDetails';
 
 const App = () => {
+    const user = JSON.parse(localStorage.getItem('profile'));
 
     return (
         <BrowserRouter>
@@ -19,7 +20,7 @@ const App = () => {
                     <Route path="/posts" exact component={Home} />
                     <Route path="/posts/search" exact component={Home} />
                     <Route path="/posts/:id" component={PostDetails} />
-                    <Route path="/auth" exact component={Auth} />
+                    <Route path="/auth" exact component={() => (!user ? <Auth /> : <Redirect to="/posts" />)} />
                     <Route />
                 </Switch>
             </Container>
