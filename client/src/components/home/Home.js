0fc43fe583124e39462cc8a49f737d5912ec6fb3
MirkoZecic <55135159+MirkoZecic@users.noreply.github.com>
2021-06-21 17:00:31 +0200

    dodat search field, i inicijalizovan listener, malo promenjen styles za home

--- client/src/components/home/Home.js
+++ client/src/components/home/Home.js
@@ -1,16 +1,30 @@
 import React, { useState, useEffect } from 'react'
-import { Grow, Grid, Container, Paper } from '@material-ui/core';
+import { Grow, Grid, Container, Paper, AppBar, TextField, Button } from '@material-ui/core';
 import { useDispatch } from 'react-redux';
+import { useHistory, useLocation } from 'react-router-dom';
+import ChipInput from 'material-ui-chip-input';
+
 import { getPosts } from '../../actions/posts';
 import Posts from '../Posts/Posts';
 import Form from '../Form/Form';
 import useStyles from './styles';
 import Pagination from '../Pagination';
 
+function useQuery() {
+    return new URLSearchParams(useLocation().search);
+}
+
 const Home = () => {
 
     const classes = useStyles();
     const dispatch = useDispatch();
+    const query = useQuery();
+    const history = useHistory();
+    const page = query.get('page') || 1;
+    const searchQuery = query.get('searchQuery');
+    const [search, setSearch] = useState('');
+    const [tags, setTags] = useState([]);
+
 
     const [currentId, setCurrentId] = useState(null);
 
@@ -21,23 +35,48 @@ const Home = () => {
         dispatch(getPosts());
     }, [currentId, dispatch]);
 
+    const searchPost = () => {
+        if (search.trim()) {
+            // dispatch -> fetch search post
+
+        } else {
+            history.push('/');
+        }
+    };
+
+    const handleKeyPress = (e) => {
+        if (e.keyCode == 13) {
+            // 13 je enter
+            searchPost();
+        }
+    };
+
+    const handleAdd = (tag) => setTags([...tags, tag]);
+
+    const handleDelete = (tagToDelete) => setTags(tags.filter((tag) => tag !== tagToDelete));
+
     return (
         <Grow in>
-                <Container>
-                    <Grid className={classes.mainContainer} container justify="space-between" alignItems="stretch" spacing={3}>
-                        <Grid item xs={12} sm={7}>
-                            <Posts setCurrentId={setCurrentId}/>
-                        </Grid>
-
-                        <Grid item xs={12} sm={4}>
-                            <Form currentId={currentId} setCurrentId={setCurrentId}/>
-                            <Paper elevation={6}>
-                                <Pagination />
-                            </Paper>
-                        </Grid>
+            <Container maxWidth="xl">
+                <Grid className={classes.mainContainer} container justify="space-between" alignItems="stretch" spacing={3} className={classes.gridContainer}>
+                    <Grid item xs={12} sm={6} md={9}>
+                        <Posts setCurrentId={setCurrentId} />
                     </Grid>
-                </Container>
-            </Grow>
+
+                    <Grid item xs={12} sm={6} md={3}>
+                        <AppBar className={classes.appBarSaerch} position="static" color="inherit">
+                            <TextField name="search" variant="outlined" label="Search Memories" onKeyPress={handleKeyPress} fullWidth value={search} onChange={(e) => { setSearch(e.target.value) }} />
+                            <ChipInput styles={{ margin: '10px 0' }} value={tags} onAdd={handleAdd} onDelete={handleDelete} label="Search Tags" variant="outlined" />
+                            <Button onclick={searchPost} className={classes.searchButton} variant="contained" color="primary">Search</Button>
+                        </AppBar>
+                        <Form currentId={currentId} setCurrentId={setCurrentId} />
+                        <Paper elevation={6}>
+                            <Pagination />
+                        </Paper>
+                    </Grid>
+                </Grid>
+            </Container>
+        </Grow>
     )
 }
 
