d40fcc433ef59391d8fea6c59fcf00e264baa445
iNikollace <niikollace@gmail.com>
2021-06-24 15:47:21 +0200

    Update user.js

--- server/models/user.js
+++ server/models/user.js
@@ -4,7 +4,7 @@ const userSchema = mongoose.Schema({
     name: { type: String, required: true },
     email: { type: String, required: true },
     password: { type: String, required: true },
-    admin: { type: Boolean } ,
+    admin: { type: Boolean },
     id: { type: String }
 });
 
