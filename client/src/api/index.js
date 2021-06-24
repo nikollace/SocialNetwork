78dd9efd41d6df4f54fd69f9392a09d739245068
iNikollace <niikollace@gmail.com>
2021-06-24 09:45:01 +0200

    Dodato

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5000' });
+const API = axios.create({ baseURL: 'https://social-network-by-hakeri-2.herokuapp.com/' });
 
 API.interceptors.request.use((req) => {
     if (localStorage.getItem('profile')) {
